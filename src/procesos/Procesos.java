27a3ef5ef5b107be0d0cbb31be90c0ef455e3662
Jairo <Jairo@172.18.91.191>
2014-06-05 15:45:21 -0500

    procesos version 3 = creacion de metodos

--- src/procesos/Procesos.java
+++ src/procesos/Procesos.java
@@ -6,10 +6,13 @@
 
 package procesos;
 
-import javax.swing.JOptionPane;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
  
   
 /**
@@ -26,10 +29,59 @@ public class Procesos {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
+    static String directorio ="C:\\Users\\Jairo\\Desktop//DeberProcesos.txt";
+    public static void main(String[] args) throws IOException {
+    int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero : \n 1.Ingresar datos en un archivo \n 2.Leer el archivo \n  "));
+    
+   
+        if(numero ==1 ){ 
+              IngresarDatos();
+        }else{
+         if(numero ==2){
+             LeerArchivo();
+         }
+        }    
        
         // TODO code application logic here
     }
-    
+    public static void IngresarDatos() throws IOException{
+          File documento = new File(directorio);
+    if (!documento.exists()) {
+              BufferedWriter datos = new BufferedWriter(new FileWriter(directorio));
+             datos.write("************ DATOS PERSONALES DEL ESTUDIANTE  ************" + "\n" );
+              datos.write("Nombres : Jairo Contreras " + "\n" );
+              datos.write("Apellidos: Contreras Mendieta"+ "\n");
+              datos.write("Carrera:   Sistemas Informaticos y Computacion"+ "\n");
+              datos.write("Ciclo: 10mo Paralelo B " +  "\n" );
+              datos.write("Materia: Ingenieria en Procesos " +  "\n" );
+              datos.write("Correo Electronico : jacontreras@utpl.edu.ec " +  "\n" );
+
+              datos.close();
+               
+             //mensaje de creacion del archivo
+               System.out.println("datos ingresados correctamente...");
+           }else{
+             //mensaje en caso de error de creacion de archivo
+               System.out.println("Ya existe el documento");
+            
+           }
+      }
+   public static void LeerArchivo(){
+         BufferedReader br = null;
+ 		try {
+ 			String Linea;
+ 			br = new BufferedReader(new FileReader(directorio));
+ 			while ((Linea = br.readLine()) != null) { //recorremos y presentamos el contenido del archivo de texto
+                             System.out.println(Linea); // imprimimos cada linea del archivo
+ 			}  
+ 		} catch (IOException e) {
+ 			e.printStackTrace();
+ 		} finally {
+ 			try {
+ 				if (br != null)br.close();
+ 			} catch (IOException ex) {
+ 				ex.printStackTrace();
+ 			}
+ 		}
+     }
 }
