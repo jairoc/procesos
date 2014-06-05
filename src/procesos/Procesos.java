a5debe524d85f7adf814bbd65879d93299db08f7
Jairo <Jairo@172.18.91.191>
2014-06-05 14:55:35 -0500

    procesos version2= declaracion de variables

--- src/procesos/Procesos.java
+++ src/procesos/Procesos.java
@@ -6,16 +6,29 @@
 
 package procesos;
 
+import javax.swing.JOptionPane;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+ 
+  
 /**
  *
  * @author Jairo
  */
 public class Procesos {
 
+    String nombres ="Jairo Antonio";
+    String apellido ="Contreras Mendieta";
+    int edad= 26;
+    String correo = "jacontreras@utpl.edu.ec";
+    String materia ="ingenieria en procesos";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+       
         // TODO code application logic here
     }
     
